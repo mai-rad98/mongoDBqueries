a28d99980653d7d9b80fd22b435e87114df12ba6
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-12-17 21:02:34 +0200

    Connected database

--- Server.js
+++ Server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const Port = 4000
+
+// Connection URI
+const uri = 'mongodb+srv://admin:admin01@cluster0.tzb67s4.mongodb.net/customers?retryWrites=true&w=majority'
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri);
+
+// Get the default connection
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+
+
+
 
 
 app.listen(Port, () => {
